b3e79d6d037f04e3fdac6b138ee82c842247163e
tdiede <tdiede@gmail.com>
2017-02-25 16:17:06 -0800

    Added photos into bubbles.

--- bubble.js
+++ bubble.js
@@ -3,25 +3,26 @@
 
 (function() {
 
-let margins = {top:20, right:20, bottom:20, left:30};
-let width = 800 - margins.left - margins.right;
-let height = 600 - margins.top - margins.bottom;
+let width = 800;
+let height = 600;
 
 let svgBubble = d3.select('body')
     .append('svg')
-    .attr('width', width + margins.left + margins.right)
-    .attr('height', height + margins.top + margins.bottom)
+    .attr('width', width)
+    .attr('height', height)
     .append('g')
-    .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
+    .attr('transform', 'translate(' + width/2 + ',' + height/2 + ')');
 
 // simulation is a collection of forces
 // about where we want circles to go
 // and how we want them to interact
 let simulation = d3.forceSimulation()
     // .force('name', defineForce)
-    .force('x', d3.forceX(width/2).strength(0.05))
-    .force('y', d3.forceY(height/2).strength(0.05))
-    .force('collide', d3.forceCollide(function(d) { return radiusScale(d.firstname.length**2); }));
+    .force('x', d3.forceX().strength(0.05))
+    .force('y', d3.forceY().strength(0.05))
+    .force('collide', d3.forceCollide(function(d) { return radiusScale(d.firstname.length**2 + 3); }));
+
+let defs = svgBubble.append('defs');
 
 let radiusScale = d3.scaleSqrt()
     .domain([1,300])
@@ -44,7 +45,25 @@ function ready(error, data) {
         .attr('cx', 130)
         .attr('cy', 100)
         .attr('r', function(d) { return radiusScale(d.firstname.length**2); })
-        .attr('fill', 'lightblue');
+        // .attr('fill', 'lightblue')
+        .attr('fill', function(d) { return 'url(#' + d.bioid + ')'; })
+        .on('click', function(d) { console.log(d); });
+
+    defs.selectAll('.pattern')
+        .data(data)
+        .enter()
+        .append('pattern')
+        .attr('class', 'pattern')
+        .attr('id', function(d) { return d.bioid; })
+        .attr('height','100%')
+        .attr('width','100%')
+        .attr('patternContentUnits','objectBoundingBox')
+        .append('image')
+            .attr('height', 1)
+            .attr('width', 1)
+            .attr('preserveAspectRatio', 'none')
+            .attr('xmlns:xlink', 'http://www.w3.org/1999/xlink')
+            .attr('xlink:href', function(d) { return '225x275/' + d.bioid + '.jpg'; });
 
     simulation.nodes(data)
         .on('tick', tick);
@@ -54,6 +73,15 @@ function ready(error, data) {
             .attr('cx', function(d) { return d.x; })
             .attr('cy', function(d) { return d.y; });
     }
+
+    circles.append('text')
+        .data(data)
+        .enter()
+        .text(function(d) { return d.lastname; })
+        .attr('x', width/2)
+        .attr('y', height/2)
+        .style('fill', 'black')
+        .style('text-anchor', 'middle');
 
 }
 
