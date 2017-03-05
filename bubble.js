af580375368e77149f3d1ac5b188ea16d3186f38
tdiede <tdiede@gmail.com>
2017-03-04 17:32:34 -0800

    Tenure data interaction.

--- bubble.js
+++ bubble.js
@@ -7,7 +7,7 @@ const width = 1200;
 const height = 800;
 
 const title = "WHO'S WHO in the U.S. SENATE";
-const subtitle = "115th Congress";
+const subtitle = "115th Congress: 2017 – 2019";
 
 const presentYear = new Date().getFullYear();
 
@@ -29,24 +29,23 @@ let tip = d3.select('body')
 
 // forces
 let forceX = d3.forceX( width/2 ).strength(0.1);
-let forceY = d3.forceY( height/2 ).strength(0.1);
+let forceY = d3.forceY( height/2 ).strength(0.12);
 let forceCollide = d3.forceCollide(function(d) {
         let yearsOffice = presentYear - d.assumed;
-        return radiusScale(yearsOffice) + padding+5; }).strength(0.9);
-// let attractForce = d3.forceManyBody().strength(200).distanceMax(400).distanceMin(60);
+        return radiusScale(yearsOffice) + padding*2; }).strength(0.9);
+let attractForce = d3.forceManyBody().strength(20).distanceMax(1000).distanceMin(100);
 let repelForce = d3.forceManyBody().strength(-140).distanceMax(500).distanceMin(20);
 // simulation is a collection of forces about where circles go and how they interact
 let simulation = d3.forceSimulation()  // .force('name', defineForce)
     .force('x', forceX)
     .force('y', forceY)
-    // .force('attract', attractForce)
+    .force('attract', attractForce)
     .force('repel', repelForce)
     .force('collide', forceCollide)
-    .alpha(0.5)
-                .alphaTarget(0.25)
-            .alphaDecay(0.1)
-            .velocityDecay(0.3);
-            console.log('up555fdte');
+    .alpha(1)
+    .alphaTarget(0)
+    .alphaDecay(0.01)
+    .velocityDecay(0.3);
 
 let photoFill = svg.append('defs');
 let hatchOverlay = svg.append('defs');
@@ -157,12 +156,10 @@ function ready(error, data) {
         .on("mouseout", leave);
 
     simulation.nodes(data)
-        .on('tick', tick)
+        .on('tick', ticked)
         .on('end', function() { console.log('end reached'); });
 
-    // setTimeout(function() { simulation.stop(); }, 2000);
-
-    function tick() {
+    function ticked() {
         photos.attr('cx', function(d) { return d.x; }).attr('cy', function(d) { return d.y; });
         hatches.attr('cx', function(d) { return d.x; }).attr('cy', function(d) { return d.y; });
         borders.attr('cx', function(d) { return d.x; }).attr('cy', function(d) { return d.y; });
@@ -182,7 +179,7 @@ function ready(error, data) {
         .attr('dy', +50)
         .style('text-anchor', 'left');
 
-    const labels = ['party', 'class', 'tenure', 'gender'];
+    const labels = ['party', 'class', 'tenure', 'gender', 'mix'];
 
     // interaction with bubbles based on data attributes
     let controls = svg.selectAll('.interface')
@@ -190,91 +187,186 @@ function ready(error, data) {
         .append('g')
         .attr('class', 'interface');
     controls.append('text')
-        .text(function(d) { return d.toUpperCase(); })
+        .text(function(d) { if(!(d==='mix')) { return d.toUpperCase(); } else { return 'mix `em all up'; }})
         .attr('id', function(d) { return d; })
         .attr('dx', +20)
         .attr('dy', function(d,i) { return (i*25)+100; })
         .style('text-anchor', 'left');
 
+
+
+    // CLICK EVENT
+    // initialize array to store current selected parameters
     let currentParameters = [];
     controls.on('click', function(d, i) {
         console.log(d);
 
-        let labelPos = +d3.select('text[id='+d+']').attr('dx');
-        if(labelPos === +30) {
-            let pos = currentParameters.indexOf(d);
-            currentParameters.splice(pos, 1);
-            evalForces(d);
-            d3.select('text[id='+d+']')
-                .transition()
-                    .attr('dx', +20)
-                    .style('fill', '#333333');
-        } else if(labelPos === +20) {
-            currentParameters.push(d);
-            evalForces(d);
-            d3.select('text[id='+d+']')
-                .transition()
-                    .attr('dx', +30)
-                    .style('fill', function(d) {
-                        if(d === 'party') { return 'slateblue'; }
-                        else if(d === 'class') { return 'tomato'; }
-                        else if(d === 'tenure') { return 'turquoise'; }
-                        else if(d === 'gender') { return 'lightpink'; }
-                    });
+        // // detect label position and update controls accordingly
+        // let labelPos = +d3.select('text[id='+d+']').attr('dx');
+        // console.log(labelPos + d);
+        // if(labelPos === +30) {
+        //     let pos = currentParameters.indexOf(d);
+        //     currentParameters.splice(pos, 1);
+        //     resetControl(d);
+        // } else if(labelPos === +20) {
+        //     currentParameters.push(d);
+        //     updateControl(d);
+        // }
+
+
+
+        switch(d) {
+            case 'mix':
+                // resetControl(d);
+                simulation.force('x', forceX)
+                          .force('y', forceY)
+                          .alpha(1).alphaTarget(0).alphaDecay(0.005).velocityDecay(0.1)
+                          .restart();
+                break;
+            case 'tenure':
+                data.sort(function(a,b) { return b.assumed - a.assumed; });
+                simulation.nodes(data)
+                    .on('tick', sortByTenure)
+                    .on('end', function() { console.log('end reached'); });
+
+
+
+                function sortByTenure() {
+                    photos.attr('cx', function(d) { return cxCalculate(d); }).attr('cy', function(d) { return cyCalculate(d); });
+                    hatches.attr('cx', function(d) { return cxCalculate(d); }).attr('cy', function(d) { return cyCalculate(d); });
+                    borders.attr('cx', function(d) { return cxCalculate(d); }).attr('cy', function(d) { return cyCalculate(d); });
+                }
         }
 
-        // forceX = d3.forceX( width/2 ).strength(0.1);
-        // forceY = d3.forceY( height/2 ).strength(0.1);
 
-        function evalForces(d) {
-            console.log(d);  // party
-            if(currentParameters.includes(d)) {
-                forceX = d3.forceX(function(d) {
-                    console.log(d);
-                    if(d.party == 'R') {
-                        return width/4;
-                    } else if (d.party == 'D') {
-                        return width*3/4;
-                    } else {
-                        return width/2;
-                    }
-                    }).strength(0.25);
-            } else {
-                forceX = d3.forceX( width/2 ).strength(0.2);
-                forceY = d3.forceY( height/2 ).strength(0.2);
-                console.log(forceX.strength,forceY,forceCollide);
-                // forceCollide = d3.forceCollide(function(d) {
-                //     let yearsOffice = presentYear - d.assumed;
-                //     return radiusScale(yearsOffice) + padding+2; }).strength(0.8);
+                const numColumns = 10;
+                const numRows = data.length/numColumns;
+
+                function cxCalculate(d) {
+                    let idx = data.indexOf(d);
+                    const offsetW = 200;
+                    const columnWidth = (width-offsetW)/numColumns;
+                    let columnNo = idx % numColumns;
+                    let cx = columnNo*columnWidth + columnWidth/2;
+                    return cx + offsetW/2;
+                }
+                function cyCalculate(d) {
+                    let idx = data.indexOf(d);
+                    const offsetH = 180;
+                    const rowHeight = ((height-offsetH)/(numRows*2)) + padding;
+                    let yearsOffice = presentYear - data[idx].assumed;
+                    let diameter = radiusScale(yearsOffice)*2;
+                    let rowNo = Math.floor(idx / numRows);
+                    let cy = rowNo*(rowHeight+(diameter/3));
+                    // let cy = rowNo*diameter;
+                    return cy + offsetH/2;
+                }
+
+
+
+        // evalForces(d);
+
+
+        // function updateControl(d) {
+        //     d3.select('text[id='+d+']').transition()
+        //         .attr('dx', +30)
+        //         .style('fill', function(d) {
+        //             if(d === 'party') { return 'slateblue'; }
+        //             else if(d === 'class') { return 'tomato'; }
+        //             else if(d === 'tenure') { return 'turquoise'; }
+        //             else if(d === 'gender') { return 'lightpink'; }
+        //         });
+        // }
+
+        function resetControl(d) {
+            switch(d) {
+                case 'mix':
+                    d3.select('text[class=interface]').transition()
+                        .attr('dx', +20)
+                        .style('fill', '#333333');
+                    break;
+                default:
+                    d3.select('text[id='+d+']').transition()
+                        .attr('dx', +20)
+                        .style('fill', '#333333');
             }
-            // else if(d === 'tenure') { forceX = forceXtenure; }
-            // else if(d === 'gender') { forceY = forceYgender; }
-            // else if(d === 'class') { forceY = forceYclass; }
         }
+//         function evalForces(d) {
 
-        let forceYgender = d3.forceY(function(d) {
-            if(d.gender == 'F') {
-                return height*3/16;
-            } else if (d.gender == 'M') {
-                return height*5/8;
-            }
-            }).strength(0.3);
+//             console.log(d);  // party, for example
 
 
-        // let Xforces = currentParameters.map(function(p) { return 'forceX' + p; });
-        // let Yforces = currentParameters.map(function(p) { return 'forceY' + p; });
 
-        // for(let i in currentParameters) { console.log(currentParameters[i]); }
-        // currentParameters.forEach(function(parameter) { });
 
-        simulation
-            .force('x', forceX)
-            .force('y', forceY)
-            .force('collide', forceCollide)
-            .alphaTarget(0.25)
-            .alphaDecay(0.1)
-            .velocityDecay(0.1)
-            .restart();
+//             forceX = d3.forceX( width/2 ).strength(0.1);
+//             forceY = d3.forceY( height/2 ).strength(0.12);
+
+//             let forceXparty = d3.forceX(function(d) {
+//                 if(d.party == 'R') { return width/4; }
+//                 else if (d.party == 'D') { return width*3/4; }
+//                 else { return width/2; }
+//                 }).strength(0.25);
+
+
+
+//             let forceYgender = d3.forceY(function(d) {
+//                 if(d.gender == 'F') { return height*4/16; }
+//                 else if (d.gender == 'M') { return height*5/8; }
+//                 }).strength(0.3);
+
+
+
+
+
+
+// forceX = d3.forceX( width/2 ).strength(0.1);
+// forceY = d3.forceY( height/2 ).strength(0.12);
+
+
+
+
+// switch (currentParameters.join(' ')) {
+    
+
+//     case 'party gender':
+//         console.log('first case will split by party and gender' + d);
+// forceX = forceXparty;
+// forceY = forceYgender;
+//         break;
+//     case 'gender':
+//         console.log('2nd case just gender, remove' + d);
+// forceY = forceYgender;
+// forceX = forceX;
+//         break;
+//     case 'party':
+// forceX = forceXparty;
+// forceY = forceY;
+//         console.log('3rd case just party, remove' + d);
+//         break;
+//     case 'tenure':
+//         console.log('4th case' + d);
+//         break;
+//     default:
+//     forceX = forceX;
+//     forceY = forceY;
+
+// }
+
+
+
+//                 simulation
+//                     .force('x', forceX)
+//                     .force('y', forceY)
+//                     // .force('collide', forceCollide)
+//                     .alpha(1)
+//                     .alphaTarget(0)
+//                     .alphaDecay(0.005)
+//                     .velocityDecay(0.4)
+//                     .restart();
+//                 console.log('dsfj');
+//                 }
+
+//         }
 
         });
 
